ad3dd401d66847784685337c42fe52a2d28db1e5
James E. Marca <james@activimetrics.com>
2017-08-18 08:44:36 -0700

    initial commit

---
+++ lib/community_web/router.ex
@@ -0,0 +1,18 @@
+defmodule CommunityWeb.Router do
+   use CommunityWeb, :router
+
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/"  do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: Community.Web.Endpoint}
+  end
+
+end
